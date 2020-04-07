2ddbc055bcd18d26acb7619727753242ec773894
Zorball24 <62960455+Zorball24@users.noreply.github.com>
2020-04-07 18:56:51 +0200

    Commentaire ajouté

--- exo_05.rb
+++ exo_05.rb
@@ -1,22 +1,22 @@
-puts "On va compter le nombre d'heures de travail à THP"
-puts "Travail : #{10 * 5 * 11}"
-puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+puts "On va compter le nombre d'heures de travail à THP" #c'est assez clair
+puts "Travail : #{10 * 5 * 11}" #on ecrit "Travail : " suivi du résultat de 10 * 5 *11
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #on ecrit "En minutes ça fait : " suivi du résultat de 10 * 5 *11 * 60
 
-puts "Et en secondes ?"
+puts "Et en secondes ?" #c'est assez clair
 
-puts 10 * 5 * 11 * 60 * 60
+puts 10 * 5 * 11 * 60 * 60 #on ecrit le résultat de 10 * 5 * 11 * 60 *60
 
-puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #c'est assez clair
 
-puts 3 + 2 < 5 - 7
+puts 3 + 2 < 5 - 7 #on teste si 3 + 2 est inférieur 5 - 7 et on écrit si c'est vrai ou faux
 
-puts "Ça fait combien 3 + 2 ? #{3 + 2}"
-puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" #on ecrit "Ça fait combien 3 + 2 ? " suivi du résultat de 3 + 2
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" #on ecrit "Ça fait combien 5 - 7 ? " suivi du résultat de 5 - 7
 
-puts "Ok, c'est faux alors !"
+puts "Ok, c'est faux alors !" #c'est assez clair
 
-puts "C'est drôle ça, faisons-en plus :"
+puts "C'est drôle ça, faisons-en plus :" #c'est assez clair
 
-puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
-puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
-puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #on ecrit "Est-ce que 5 est plus grand que -2 ?" on teste si 5 est supérieur -2 et on écrit si c'est vrai ou faux
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #on ecrit "Est-ce que 5 est supérieur ou égal à -2 ?" on teste si 5 est inférieur -2 et on écrit si c'est vrai ou faux
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #on ecrit "Est-ce que 5 est inférieur ou égal à -2 ?"on teste si 5 est inférieur -2 et on écrit si c'est vrai ou faux
